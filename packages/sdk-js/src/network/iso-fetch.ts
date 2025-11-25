1e041fb8a7bb9c9ac7bf215afd8ad3a03d94507c
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2025-11-25 09:51:23 +0100

    Use a safe integer as max timeout
    Closes Cannot use with Node.js
    Fixes #4

--- packages/sdk-js/src/network/iso-fetch.ts
+++ packages/sdk-js/src/network/iso-fetch.ts
@@ -1,4 +1,6 @@
-import { getEnv } from '../helpers.js'
+import { getEnv } from '../helpers.js';
+
+const MAX_TIMEOUT = 2147483647;
 
 export async function initFetch(): Promise<typeof fetch> {
   if (getEnv('VITEST_WORKER_ID')) {
@@ -14,9 +16,9 @@ export async function initFetch(): Promise<typeof fetch> {
     // @ts-expect-error undici is an optional dependency
     const { fetch: uFetch, Agent } = await import('undici')
     const agent = new Agent({
-      connectTimeout: Number.MAX_SAFE_INTEGER,
-      connect: { timeout: Number.MAX_SAFE_INTEGER },
-      headersTimeout: Number.MAX_SAFE_INTEGER,
+      connectTimeout: MAX_TIMEOUT,
+      connect: { timeout: MAX_TIMEOUT },
+      headersTimeout: MAX_TIMEOUT,
       bodyTimeout: 0,
     })
     return (url: string | URL | Request, init?: RequestInit | undefined) => {
